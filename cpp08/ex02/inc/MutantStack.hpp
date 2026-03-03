164c42e8a4a9e0e8759cfad976466abef6656600
lgracia- <lgracia-@car4s6.42barcelona.com>
2026-03-03 13:44:48 +0100

    cpp08

--- cpp08/ex02/inc/MutantStack.hpp
+++ cpp08/ex02/inc/MutantStack.hpp
@@ -2,7 +2,6 @@
 # define MUTANTSTACK_HPP
 
 #include <iostream>
-#include <string>
 #include <stack>
 
 template <typename T>
@@ -11,12 +10,15 @@ class MutantStack : public std::stack<T>
 	public:
 		MutantStack() : std::stack<T>() {};
 		MutantStack(const MutantStack& other) : std::stack<T>(other) {};
-		MutantStack& operator=(const MutantStack& other) { std::stack<T>::operator=(other); return *this; };
+		MutantStack& operator=(const MutantStack& other) { if (this != &other) std::stack<T>::operator=(other); return *this; };
 		~MutantStack(){};
 		
 		typedef typename std::stack<T>::container_type::iterator iterator;
+		typedef typename std::stack<T>::container_type::const_iterator const_iterator;
 		iterator begin() { return (this->c.begin()); };
 		iterator end() { return (this->c.end()); };
+		const_iterator begin() const { return (this->c.begin()); };
+		const_iterator end() const { return (this->c.end()); };
 };
 
 #endif
--- cpp08/ex02/src/main.cpp
+++ cpp08/ex02/src/main.cpp
@@ -1,20 +1,44 @@
 #include "MutantStack.hpp"
 #include <list>
+#include <vector>
 
 int main()
 {
-	// try
-	// {
-	// 	std::stack<int> sta;
-	// 	std::cout << sta.top() << std::endl;
-
-	// }
-	// catch(const std::exception& e)
-	// {
-	// 	std::cerr << e.what() << std::endl << std::endl;
-
-	// }
 	{
+		std::cout << "\033[30m	My tests\033[0m" << std::endl;
+		std::cout << "\033[30m		Const_iterator\033[0m" << std::endl;
+		MutantStack<int> mstack;
+		mstack.push(5);
+		mstack.push(17);
+		std::cout << mstack.top() << std::endl;
+		mstack.pop();
+		std::cout << mstack.size() << std::endl;
+		mstack.push(3);
+		mstack.push(5);
+		mstack.push(737);
+		mstack.push(0);
+		MutantStack<int>::const_iterator it = mstack.begin();
+		MutantStack<int>::const_iterator ite = mstack.end();
+		std::cout << "iteartor  at begin is = " << *it << std::endl;
+		std::cout << "top is = " << mstack.top() << std::endl;
+		++it;
+		--it;
+		while (it != ite)
+		{
+		std::cout << *it << std::endl;
+		++it;
+		}
+		std::cout << "\033[30m		Iterator modification\033[0m" << std::endl;
+		MutantStack<int> s(mstack);
+		MutantStack<int>::iterator i = s.begin();
+		std::cout << *i << std::endl;
+		*i = 42;
+		std::cout << *i << std::endl;
+		std::cout << std::endl;
+	}
+	{
+		std::cout << "\033[30m	Default\033[0m" << std::endl;
+		std::cout << "\033[30m		MutantStack\033[0m" << std::endl;
 		MutantStack<int> mstack;
 		mstack.push(5);
 		mstack.push(17);
@@ -39,10 +63,35 @@ int main()
 		std::cout << std::endl;
 	}
 	{
+		std::cout << "\033[30m		Vector\033[0m" << std::endl;
+		std::vector<int> mstack;
+		mstack.push_back(5);
+		mstack.push_back(17);
+		std::cout << mstack.back() << std::endl;
+		mstack.pop_back();
+		std::cout << mstack.size() << std::endl;
+		mstack.push_back(3);
+		mstack.push_back(5);
+		mstack.push_back(737);
+		//[...]
+		mstack.push_back(0);
+		std::vector<int>::iterator it = mstack.begin();
+		std::vector<int>::iterator ite = mstack.end();
+		++it;
+		--it;
+		while (it != ite)
+		{
+		std::cout << *it << std::endl;
+		++it;
+		}
+		std::vector<int> s(mstack);
+	}
+	{
+		std::cout << "\033[30m		List\033[0m" << std::endl;
 		std::list<int> mstack;
 		mstack.push_back(5);
 		mstack.push_back(17);
-		std::cout << mstack.front() << std::endl;
+		std::cout << mstack.back() << std::endl;
 		mstack.pop_back();
 		std::cout << mstack.size() << std::endl;
 		mstack.push_back(3);
