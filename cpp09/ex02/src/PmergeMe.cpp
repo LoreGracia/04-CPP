2587fd5ad13eee1173a0054c6244b590b095fcd1
lgracia- <lgracia-@car10s1.42barcelona.com>
2026-04-18 19:13:06 +0200

    identation issue

--- cpp09/ex02/src/PmergeMe.cpp
+++ cpp09/ex02/src/PmergeMe.cpp
@@ -1,11 +1,11 @@
 #include "PmergeMe.hpp"
 
-//debug utils
+// debug utils
 
-void print_pent(std::vector< std::vector <int> > pent)
+void print_pent(std::vector<std::vector<int>> pent)
 {
 	std::cout << "PENT: ";
-	for (std::vector< std::vector <int> >::iterator it = pent.begin(); it != pent.end(); it++)
+	for (std::vector<std::vector<int>>::iterator it = pent.begin(); it != pent.end(); it++)
 		std::cout << (*it)[0] << " ";
 	std::cout << std::endl;
 }
@@ -27,11 +27,15 @@ void print_v_t(std::vector<size_t> v, std::string str)
 }
 
 PmergeMe::PmergeMe() {}
-PmergeMe::PmergeMe(const PmergeMe& other) { *this = other; }
-PmergeMe& PmergeMe::operator=(const PmergeMe& other)  { _res = other._res; return *this; }
+PmergeMe::PmergeMe(const PmergeMe &other) { *this = other; }
+PmergeMe &PmergeMe::operator=(const PmergeMe &other)
+{
+	_res = other._res;
+	return *this;
+}
 PmergeMe::~PmergeMe() {}
 
-int	PmergeMe::parse(char **av)
+int PmergeMe::parse(char **av)
 {
 	if (!av || !av[0] || !av[0][0])
 		throw std::logic_error("empty input");
@@ -50,158 +54,161 @@ int	PmergeMe::parse(char **av)
 // ---------------- Jacobsthal ----------------
 std::vector<size_t> PmergeMe::jacobsthalSequence(size_t n)
 {
-    std::vector<size_t> j;
-    j.push_back(0);
-    if (n == 0)
+	std::vector<size_t> j;
+	j.push_back(0);
+	if (n == 0)
 		return j;
 
-    j.push_back(1);
-    while (j.back() < n)
+	j.push_back(1);
+	while (j.back() < n)
 	{
-        size_t s = j.size();
-        j.push_back(j[s - 1] + 2 * j[s - 2]);
-    }
-    return j;
+		size_t s = j.size();
+		j.push_back(j[s - 1] + 2 * j[s - 2]);
+	}
+	return j;
 }
 
 // ---------------- Binary insert con índice límite ----------------
-size_t PmergeMe::binarySearchIndex(const std::vector<int>& arr, int value, size_t end)
+size_t PmergeMe::binarySearchIndex(const std::vector<int> &arr, int value, size_t end)
 {
-    size_t left = 0;
-    size_t right = end;
+	size_t left = 0;
+	size_t right = end;
 
-    while (left < right)
+	while (left < right)
 	{
-        size_t mid = (left + right) / 2;
-        if (arr[mid] < value)
-            left = mid + 1;
-        else
-            right = mid;
-    }
-    return left;
+		size_t mid = (left + right) / 2;
+		if (arr[mid] < value)
+			left = mid + 1;
+		else
+			right = mid;
+	}
+	return left;
 }
 
 // ---------------- Ford-Johnson ----------------
 std::vector<int> PmergeMe::fordJohnson(std::vector<int> input)
 {
-    if (input.size() <= 1)
-        return input;
-    // --- pairs
-    std::vector< std::pair<int,int> > pairs;
+	if (input.size() <= 1)
+		return input;
+	// --- pairs
+	std::vector<std::pair<int, int>> pairs;
 
-    for (size_t i = 0; i + 1 < input.size(); i += 2)
+	for (size_t i = 0; i + 1 < input.size(); i += 2)
 	{
-        if (input[i] > input[i + 1])
-            pairs.push_back(std::make_pair(input[i], input[i + 1]));
-        else
-            pairs.push_back(std::make_pair(input[i + 1], input[i]));
-    }
+		if (input[i] > input[i + 1])
+			pairs.push_back(std::make_pair(input[i], input[i + 1]));
+		else
+			pairs.push_back(std::make_pair(input[i + 1], input[i]));
+	}
 
-	//odd number 
-    int odd = 0;
-    bool isOdd = input.size() % 2;
-    if (isOdd)
-        odd = input.back();
+	// odd number
+	int odd = 0;
+	bool isOdd = input.size() % 2;
+	if (isOdd)
+		odd = input.back();
 
-    // --- winners
-    std::vector<int> winners;
-    for (size_t i = 0; i < pairs.size(); ++i)
-        winners.push_back(pairs[i].first);
+	// --- winners
+	std::vector<int> winners;
+	for (size_t i = 0; i < pairs.size(); ++i)
+		winners.push_back(pairs[i].first);
 
-    winners = fordJohnson(winners);
+	winners = fordJohnson(winners);
 
-    // --- update first result
-    std::vector<int> result = winners;
+	// --- update first result
+	std::vector<int> result = winners;
 
-    // 🔑 positions of winners (index)
-    std::vector<size_t> winnerPos(pairs.size());
+	// 🔑 positions of winners (index)
+	std::vector<size_t> winnerPos(pairs.size());
 
-    for (size_t i = 0; i < pairs.size(); ++i)
+	for (size_t i = 0; i < pairs.size(); ++i)
 	{
-        for (size_t j = 0; j < result.size(); ++j)
+		for (size_t j = 0; j < result.size(); ++j)
 		{
-            if (result[j] == pairs[i].first)
+			if (result[j] == pairs[i].first)
 			{
-                winnerPos[i] = j;
-                break;
-            }
-        }
-    }
+				winnerPos[i] = j;
+				break;
+			}
+		}
+	}
 
-    // --- losers
-    std::vector<int> losers;
-    for (size_t i = 0; i < pairs.size(); ++i)
-        losers.push_back(pairs[i].second);
+	// --- losers
+	std::vector<int> losers;
+	for (size_t i = 0; i < pairs.size(); ++i)
+		losers.push_back(pairs[i].second);
 
-    std::vector<size_t> jac = jacobsthalSequence(losers.size());
-    std::vector<bool> inserted(losers.size(), false);
-    // insertion
-    for (size_t i = 1; i < jac.size(); ++i)
+	std::vector<size_t> jac = jacobsthalSequence(losers.size());
+	std::vector<bool> inserted(losers.size(), false);
+	// insertion
+	for (size_t i = 1; i < jac.size(); ++i)
 	{
-        size_t idx = jac[i];
+		size_t idx = jac[i];
+		// std::cout << idx << std::endl;
+		if (idx >= losers.size())
+			continue;
+
+		std::cout << i << " ";
 		std::cout << idx << std::endl;
-        if (idx >= losers.size())
-            continue;
-
-        if (!inserted[idx])
+		if (!inserted[idx])
 		{
-            size_t pos = binarySearchIndex(result, losers[idx], winnerPos[idx]);
+			std::cout << "primero" << std::endl;
+			size_t pos = binarySearchIndex(result, losers[idx], winnerPos[idx]);
 
-            result.insert(result.begin() + pos, losers[idx]);
+			result.insert(result.begin() + pos, losers[idx]);
 
-            // update postions
-            for (size_t k = 0; k < winnerPos.size(); ++k)
-                if (winnerPos[k] >= pos)
-                    winnerPos[k]++;
+			// update postions
+			for (size_t k = 0; k < winnerPos.size(); ++k)
+				if (winnerPos[k] >= pos)
+					winnerPos[k]++;
 
-            inserted[idx] = true;
-        }
+			inserted[idx] = true;
+		}
 
-        for (int j = (int)idx - 1; j >= 0; --j)
+		for (int j = (int)idx - 1; j >= 0; --j)
 		{
-            if (!inserted[j])
+			if (!inserted[j])
 			{
-                size_t pos = binarySearchIndex(result, losers[j], winnerPos[j]);
+				size_t pos = binarySearchIndex(result, losers[j], winnerPos[j]);
 
-                result.insert(result.begin() + pos, losers[j]);
+				result.insert(result.begin() + pos, losers[j]);
 
-                for (size_t k = 0; k < winnerPos.size(); ++k)
-                    if (winnerPos[k] >= pos)
-                        winnerPos[k]++;
+				for (size_t k = 0; k < winnerPos.size(); ++k)
+					if (winnerPos[k] >= pos)
+						winnerPos[k]++;
 
-                inserted[j] = true;
-            }
-        }
-    }
+				inserted[j] = true;
+			}
+		}
+	}
 
-    // lefts out
-    for (size_t i = 0; i < losers.size(); ++i)
+	// lefts out
+	for (size_t i = 0; i < losers.size(); ++i)
 	{
-        if (!inserted[i])
+		if (!inserted[i])
 		{
-            size_t pos = binarySearchIndex(result, losers[i], winnerPos[i]);
+			size_t pos = binarySearchIndex(result, losers[i], winnerPos[i]);
 
-            result.insert(result.begin() + pos, losers[i]);
+			result.insert(result.begin() + pos, losers[i]);
 
-            for (size_t k = 0; k < winnerPos.size(); ++k)
-                if (winnerPos[k] >= pos)
-                    winnerPos[k]++;
-        }
-    }
+			for (size_t k = 0; k < winnerPos.size(); ++k)
+				if (winnerPos[k] >= pos)
+					winnerPos[k]++;
+		}
+	}
 
-    // odd
-    if (isOdd)
+	// odd
+	if (isOdd)
 	{
-        size_t pos = binarySearchIndex(result, odd, result.size());
-        result.insert(result.begin() + pos, odd);
-    }
+		size_t pos = binarySearchIndex(result, odd, result.size());
+		result.insert(result.begin() + pos, odd);
+	}
 
-    return result;
+	return result;
 }
 
-//public
+// public
 
-void	PmergeMe::calculate(char **av)
+void PmergeMe::calculate(char **av)
 {
 	if (parse(av))
 		throw std::logic_error("invalid input");
@@ -211,7 +218,7 @@ void	PmergeMe::calculate(char **av)
 
 std::vector<int> PmergeMe::getRes() const { return _res; }
 
-std::ostream& operator<<(std::ostream& out, const PmergeMe& pm)
+std::ostream &operator<<(std::ostream &out, const PmergeMe &pm)
 {
 	std::vector<int> res = pm.getRes();
 	for (std::vector<int>::iterator i = res.begin(); i != res.end(); i++)
