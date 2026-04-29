2761381b3cad0474d63abe7a68af0f4541884ae4
lgracia- <lgracia-@car12s2.42barcelona.com>
2026-04-29 15:12:32 +0200

    colors for cpp09/ex01

--- cpp09/ex01/src/main.cpp
+++ cpp09/ex01/src/main.cpp
@@ -7,15 +7,20 @@ int main(int argc, char **argv)
 	try
 	{
 		RPN rpn;
-		std::cout << rpn.calculate("8 9 * 9 - 9 - 9 - 4 - 1 +") << std::endl;
-		std::cout << rpn.calculate("7 7 * 7 -") << std::endl;
-		std::cout << rpn.calculate("1 2 * 2 / 2 * 2 4 - +") << std::endl;
-		std::cout << rpn.calculate("(1 + 1)") << std::endl;
+		std::string a = "8 9 * 9 - 9 - 9 - 4 - 1 +";
+		std::string b = "7 7 * 7 -";
+		std::string c = "1 2 * 2 / 2 * 2 4 - +";
+		std::string d = "(1 + 1)";
+		std::cout << "\033[30m" << "\"" << a << "\" =	\033[0m" << rpn.calculate(a) << std::endl;
+		std::cout << "\033[30m" << "\"" << b << "\" =	\033[0m" << rpn.calculate(b) << std::endl;
+		std::cout << "\033[30m" << "\"" << c << "\" =	\033[0m" << rpn.calculate(c) << std::endl;
+		std::cout << "\033[30m" << "\"" << d << "\" =	\033[0m" << rpn.calculate(d) << std::endl;
 	}
 	catch(const std::exception& e)
 	{
-		std::cerr << "Error: " << e.what() << '\n';
+		std::cerr << "Error: " << e.what() << "\033[0m" << std::endl;
 	}
+	std::cout << std::endl;
 	try
 	{
 		RPN rpn;
